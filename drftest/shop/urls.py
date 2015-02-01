0b6a9d08ee586e67cd9f142a3c95e9693292326b
Andrea Grandi <a.grandi@gmail.com>
2015-02-01 17:34:19 +0100

    Change tabs to spaces

--- drftest/shop/urls.py
+++ drftest/shop/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from shop.api import views
 
 urlpatterns = (
-	url(r'^orders/$', views.OrdersView.as_view(), name='shop_orders'),
-	)
+    url(r'^orders/$', views.OrdersView.as_view(), name='shop_orders'),
+    )
