3a190fb6ea09f7202bc1ae334f183cebd95e185f
Andrea Grandi <a.grandi@gmail.com>
2015-02-01 17:41:18 +0100

    Change tabs to spaces

--- drftest/shop/api/views.py
+++ drftest/shop/api/views.py
@@ -5,14 +5,14 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 class ShopAPIView(APIView):
-	permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
 
 class OrdersView(ShopAPIView):
-	"""
-	Place an order. Create the "order" record with general informations, create the "order_details" records with
-	the details of the order. During this transaction any stamp earned by the user is added to the database and
-	at the end voucher(s) are created if there are enough stamps available for that user.
-	"""
-	def post(self, request, format=None):
-		return Response({'success': True})
+    """
+    Place an order. Create the "order" record with general informations, create the "order_details" records with
+    the details of the order. During this transaction any stamp earned by the user is added to the database and
+    at the end voucher(s) are created if there are enough stamps available for that user.
+    """
+    def post(self, request, format=None):
+        return Response({'success': True})
--- drftest/shop/models.py
+++ drftest/shop/models.py
@@ -3,26 +3,26 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-	name = models.CharField(max_length=255)
-	collect_stamp = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    collect_stamp = models.BooleanField(default=False)
 
 
 class Stamp(models.Model):
-	user = models.ForeignKey(User)
-	redeemed = models.BooleanField(default=False)
+    user = models.ForeignKey(User)
+    redeemed = models.BooleanField(default=False)
 
 
 class Order(models.Model):
-	user = models.ForeignKey(User)
-	date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
 
 
 class OrderDetails(models.Model):
-	order = models.ForeignKey(Order)
-	product = models.ForeignKey(Product)
-	quantity = models.BigIntegerField()
+    order = models.ForeignKey(Order)
+    product = models.ForeignKey(Product)
+    quantity = models.BigIntegerField()
 
 
 class Voucher(models.Model):
-	user = models.ForeignKey(User)
-	redeemed = models.BooleanField(default=False)
+    user = models.ForeignKey(User)
+    redeemed = models.BooleanField(default=False)
