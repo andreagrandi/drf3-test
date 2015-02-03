c3b723da1a5bb53fac2f49ad0cbb8bdf4763c359
Andrea Grandi <a.grandi@gmail.com>
2015-02-03 22:11:04 +0000

    Fixes some docs

--- drftest/shop/api/views.py
+++ drftest/shop/api/views.py
@@ -13,9 +13,9 @@ class ShopAPIView(APIView):
 
 class OrdersView(ShopAPIView):
     """
-    Place an order. Create the "order" record with general informations, create the "order_details" 
-    records with the details of the order. During this transaction any stamp earned by the user is 
-    added to the database and at the end voucher(s) are created if there are enough stamps available 
+    Place an order. Create the "order" record with general informations, create the "order_details"
+    records with the details of the order. During this transaction any stamp earned by the user is
+    added to the database and at the end voucher(s) are created if there are enough stamps available
     for that user.
 
         POST:
